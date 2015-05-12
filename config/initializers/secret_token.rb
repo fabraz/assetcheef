add52c97dfb797069fa050a968b2763cb0d1784c
saasbook <saasbook@saasbook.(none)>
2015-05-12 16:10:02 -0300

    Project Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assetcheef::Application.config.secret_token = 'bdbf4eb14bcf19f00d2fd43c03a3019eebbea080daef5cd1e52c309e8616d2fa090cbb648b34a064a8c28b0b2f77bf478a2652bada58085c4830883bd3d86453'
