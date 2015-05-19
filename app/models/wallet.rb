b830eee0bef22a238e162988b59d5078c3f699bc
Fillipe Feitosa <fillfeitosa@gmail.com>
2015-05-19 17:39:19 -0300

    Next step of Tdd for wallet Signed-off-by: Fillipe Feitosa <fillfeitosa@gmail.com> Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,11 @@
 class Wallet < ActiveRecord::Base
 
+  attr_accessor :id, :name, :description
+ 
+  def initialize(id, name, description)
+	@name = name
+	@id = id
+	@description = description
+  end
+  
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Wallet, type: :model do
   it "create Wallet" do
-	obj_wallet = Wallet.create
+	@obj_wallet = Wallet.new(1, "MY_WALLET", "My pit of money")
 	expect(@obj_wallet).to be_a Wallet
   end
 end
