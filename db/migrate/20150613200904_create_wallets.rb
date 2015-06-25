252e50f893f5e080bfc4eca9b92e9080ea5854c3
Wilker Mesquita <wilker-mc@hotmail.com>
2015-06-25 08:32:05 -0300

    Adding liquid_income to view in wallets show, now we have some bugs to fix.
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

--- db/migrate/20150613200904_create_wallets.rb
+++ db/migrate/20150613200904_create_wallets.rb
@@ -4,6 +4,7 @@ class CreateWallets < ActiveRecord::Migration
       t.string :name
       t.string :description
       t.belongs_to :user, index: true, foreign_key: true
+      t.references :stocks, index: true
 
       t.timestamps null: false
     end
