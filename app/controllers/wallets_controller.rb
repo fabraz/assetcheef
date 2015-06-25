dd26c0253a6ad4b645b11d2ea5d3ed2a9b20c5d4
Wilker Mesquita <wilker-mc@hotmail.com>
2015-06-25 04:52:36 -0300

    Refactoring helpers and including half of the issue #16
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,5 +1,9 @@
 class WalletsController < ApplicationController
+  include StocksHelper
+  include WalletsHelper
   before_action :set_wallet, only: [:show, :edit, :update, :destroy]
+
+  helper_method :show_moving_stock
 
   # GET /wallets
   def index
@@ -8,6 +12,7 @@ class WalletsController < ApplicationController
 
   # GET /wallets/1
   def show
+    get_variable
   end
 
   # GET /wallets/new
@@ -17,6 +22,13 @@ class WalletsController < ApplicationController
 
   # GET /wallets/1/edit
   def edit
+  end
+
+  def show_moving_stock
+
+   @stock = Stock.new 
+   @stock = liquid_income
+  
   end
 
   # POST /wallets
@@ -49,6 +61,9 @@ class WalletsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_wallet
       @wallet = Wallet.find(params[:id])
+    end
+
+    def get_variable
     end
 
     # Only allow a trusted parameter "white list" through.
--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,2 +1,8 @@
 module WalletsHelper
+	include StocksHelper
+
+	def stock_moving
+		@stock = Stock.find(@wallet.id)#.where(stock_id: @stock.id)
+		movement = liquid_income
+	end
 end
