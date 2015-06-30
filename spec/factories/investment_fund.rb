6b5ba4d9106acefa10e7ecd969a44eb9b1b7423b
Fillipe Feitosa <fillfeitosa@gmail.com>
2015-06-30 05:33:44 -0300

    Finishing Investment Fund Cucumber Test for Profit Calculation
    Signed-off-by: Fillipe Feitosa <fillfeitosa@gmail.com>
    Signed-off-by: Álvaro Fernando <alvarofernandoms@gmail.com>

--- spec/factories/investment_fund.rb
+++ spec/factories/investment_fund.rb
@@ -13,6 +13,47 @@ FactoryGirl.define do
     capital 1.0
     wallet_id 1
     closing_price 110
-    closing_date "2011-10-21T18:26:43.151+00:00"  
+    closing_date "2011-10-21T18:26:43.151+00:00"
   end
+
+  factory :investfund_lowDuration, class: InvestmentFund do
+    id 2
+    name "My First Investment Fund with Low Duration"
+    buyDate "2013-02-28"
+    admTax 1.25
+    aditionalInfo "Aditional Information"
+    capital 1000.0
+    wallet_id 1
+  end
+  
+  factory :investfund_new, class: InvestmentFund do
+    id 3
+    name "My First Investment Fund with No Duration at all"
+    buyDate Date.today.to_date
+    admTax 1.0
+    aditionalInfo "Aditional Information"
+    capital 1000.0
+    wallet_id 1
+  end
+
+  factory :investfund_mediumDuration, class: InvestmentFund do
+    id 4
+    name "My First Investment Fund with Medium Duration"
+    buyDate "2014-06-30"
+    admTax 0.9
+    aditionalInfo "Aditional Information"
+    capital 200000.0
+    wallet_id 1
+  end
+  
+  factory :investfund_regularDuration, class: InvestmentFund do
+    id 5
+    name "My First Investment Fund with Regular Duration"
+    buyDate "2014-04-30"
+    admTax 0.9
+    aditionalInfo "Aditional Information"
+    capital 10000.0
+    wallet_id 1
+  end
+
 end
